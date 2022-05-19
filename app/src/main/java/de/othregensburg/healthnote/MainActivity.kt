5f5ded8e8e52acc2c4b3fd971760839752faa672
Arda K <ardakocer@live.de>
2022-05-19 20:06:24 +0200

    Changed Activities to Fragments
    Implemented fragments and navigation with a NavController for future updates

--- app/src/main/java/de/othregensburg/healthnote/MainActivity.kt
+++ app/src/main/java/de/othregensburg/healthnote/MainActivity.kt
@@ -1,41 +1,24 @@
 package de.othregensburg.healthnote
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import de.othregensburg.healthnote.data.MedicamentViewModel
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mMedViewModel: MedicamentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.fragment_main_containerview))
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = ListAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
-
-        mMedViewModel = ViewModelProvider(this)[MedicamentViewModel::class.java]
-        mMedViewModel.readAllData.observe(this) { meds ->
-            adapter.setData(meds)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_main_containerview)
+        val navController = navHostFragment?.findNavController()
+        if (navController != null) {
+            setupActionBarWithNavController(navController)
         }
 
-        val addButton = findViewById<Button>(R.id.add_button)
-
-        addButton.setOnClickListener {
-            val intent = Intent(this, AddMedicamentActivity::class.java)
-            startActivity(intent)
-        }
     }
+
 }
--- app/src/main/java/de/othregensburg/healthnote/fragments/list/ListFragment.kt
+++ app/src/main/java/de/othregensburg/healthnote/fragments/list/ListFragment.kt
@@ -1,23 +1,42 @@
 package de.othregensburg.healthnote.fragments.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import de.othregensburg.healthnote.ListAdapter
 import de.othregensburg.healthnote.R
+import de.othregensburg.healthnote.data.MedicamentViewModel
+import de.othregensburg.healthnote.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+
+    private lateinit var mMedViewModel: MedicamentViewModel
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+        val view = binding.root
 
-        view.findViewById<Button>(R.id.add_button).setOnClickListener {
+        val adapter = ListAdapter()
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        mMedViewModel = ViewModelProvider(this)[MedicamentViewModel::class.java]
+        mMedViewModel.readAllData.observe(viewLifecycleOwner) { meds ->
+            adapter.setData(meds)
+        }
+
+        binding.addButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
