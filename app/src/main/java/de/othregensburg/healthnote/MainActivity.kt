de9b50bc1e3463b7faa5c7eff82d13b37b2de03d
Arda K <ardakocer@live.de>
2022-05-29 00:28:48 +0200

    Revert "Merge branch 'feature/navDrawer' into feature/timeAlert"
    This reverts commit e34e07dbdd76f2d6d923468a4adab4506ea6e557, reversing
    changes made to 4818ef734861235d0a47b1f435133019e5380a90.

--- app/src/main/java/de/othregensburg/healthnote/MainActivity.kt
+++ app/src/main/java/de/othregensburg/healthnote/MainActivity.kt
@@ -1,48 +1,36 @@
 package de.othregensburg.healthnote
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
-import de.othregensburg.healthnote.databinding.ActivityMainBinding
 import de.othregensburg.healthnote.model.Medicament
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.listFragment, R.id.calendarFragment
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setContentView(R.layout.activity_main)
 
         createNotificationChannel()
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_main_containerview)
+        val navController = navHostFragment?.findNavController()
+        if (navController != null) {
+            setupActionBarWithNavController(navController)
+        }
     }
 
     fun scheduleAlerts(medList: List<Medicament>) {
--- app/src/main/java/de/othregensburg/healthnote/fragments/add/AddFragment.kt
+++ app/src/main/java/de/othregensburg/healthnote/fragments/add/AddFragment.kt
@@ -7,20 +7,24 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import de.othregensburg.healthnote.R
+import de.othregensburg.healthnote.databinding.FragmentAddBinding
 import de.othregensburg.healthnote.model.Medicament
 import de.othregensburg.healthnote.viewmodel.MedicamentViewModel
-import de.othregensburg.healthnote.databinding.FragmentAddBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
-class AddFragment : Fragment() {
+class AddFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private lateinit var mMedViewModel: MedicamentViewModel
+    private lateinit var repeatInterval: String
     private var _binding: FragmentAddBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -42,10 +46,19 @@ class AddFragment : Fragment() {
             val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
                 cal.set(Calendar.HOUR_OF_DAY, hour)
                 cal.set(Calendar.MINUTE, minute)
-                binding.editTextTime.text = SimpleDateFormat("HH:mm").format(cal.time)
+                binding.editTextTime.text = SimpleDateFormat("HH:mm", Locale.GERMANY).format(cal.time)
             }
             TimePickerDialog(requireContext(), timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
         }
+
+        val spinner : Spinner = binding.repeatDropDown
+        ArrayAdapter.createFromResource(requireContext(), R.array.repeat_array, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = this
+
+
 
         setHasOptionsMenu(true)
 
@@ -57,8 +70,9 @@ class AddFragment : Fragment() {
         val form = binding.addMedicineForm.text.toString()
         val dose = binding.addMedicineDose.text.toString()
         val time = binding.editTextTime.text.toString()
+        val alertBoolean = binding.fireAlertSwitch.isChecked
         if (inputCheck(name, form, dose, time)) {
-            val med = Medicament(0, name, time, form, dose)
+            val med = Medicament(0, name, time, form, dose, alertBoolean, repeatInterval)
             mMedViewModel.addMed(med)
             Toast.makeText(requireContext(), "Successfully added", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
@@ -68,7 +82,7 @@ class AddFragment : Fragment() {
     }
 
     private fun inputCheck(name: String, form: String, dose: String, time: String) : Boolean {
-        return !(TextUtils.isEmpty(name) || TextUtils.isEmpty(form) || TextUtils.isEmpty(dose) || TextUtils.isEmpty(time))
+        return !(TextUtils.isEmpty(name) || TextUtils.isEmpty(form) || TextUtils.isEmpty(dose) || TextUtils.isEmpty(time) || TextUtils.equals(time, "SET TIME"))
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -76,5 +90,15 @@ class AddFragment : Fragment() {
             findNavController().navigate(R.id.action_addFragment_to_listFragment)
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        if (p0 != null) {
+            repeatInterval = p0.getItemAtPosition(p2).toString()
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        Toast.makeText(requireContext(), "Please fill out the repeat dropdown.", Toast.LENGTH_SHORT).show()
     }
 }
